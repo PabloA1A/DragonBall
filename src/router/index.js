69b989b513594d8637d90153ec5d2f58495dcfee
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-07 19:40:11 +0100

    added routes in the router

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,36 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: LogoutView,
+      meta: { requiresAuth: true }
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/planets',
+      name: 'planets',
+      component: Planets
+    },
+    {
+      path: '/favorite',
+      name: 'favorite',
+      component: Favorite,
+      meta: { requiresAuth: true }
     },
   ],
 })
