b79851f238e998169b9fc5de3231139bd694a1e1
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-07 20:06:43 +0100

    modified AuthStore to improve login and registration logic

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -3,19 +3,41 @@ import { defineStore } from "pinia"
 
 export const useAuthStore = defineStore('auth', () => {
 
-    const userLogin = ref(
-        {
-            username: '',
-            isAuthenticated: false
-        }
-    )
+    const userLogin = ref({
+        username: '',
+        isAuthenticated: false
+    })
 
-    const user = ref(
-        [{
+    const users = ref([
+        {
             username: 'admin',
             password: 'admin',
-        }]
-    )
+        }
+    ])
 
-    return { user, userLogin}
+    const login = (username, password) => {
+        const user = users.value.find(user => user.username === username && user.password === password)
+        if (user) {
+            userLogin.value.username = user.username
+            userLogin.value.isAuthenticated = true
+            return true
+        }
+        return false
+    }
+
+    const register = (username, password) => {
+        const userExists = users.value.some(user => user.username === username)
+        if (!userExists) {
+            users.value.push({ username, password })
+            return true
+        }
+        return false
+    }
+
+    const logout = () => {
+        userLogin.value.username = ''
+        userLogin.value.isAuthenticated = false
+    }
+
+    return { users, userLogin, login, register, logout }
 })
