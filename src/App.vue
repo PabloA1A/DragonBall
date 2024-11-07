81df4cd107d269427afa260e84917b2dddd24274
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-11-07 19:47:52 +0100

    added the NavBar to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 
 </script>
 
 <template>
- 
+   <NavBar />
+  <main class="container mt-5 pt-5">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
