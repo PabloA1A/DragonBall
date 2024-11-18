db8ec3bafd0ada2a12df1a05b0e2c0d40e1f64b0
Pablo Abad <pablo_abadalvarez@hotmail.es>
2024-11-18 15:24:34 +0100

    deploy

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import './assets/main.css'
+import './src/assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from './src/App.vue'
+import router from './src/router'
 
 const app = createApp(App)
 
